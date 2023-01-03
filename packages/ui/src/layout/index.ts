fb7793ce1416cc5d7c7e01d02abfa4002438a56e
xupengfei <faberxu@gmail.com>
2023-01-03 17:33:30 +0800

    u

--- packages/ui/src/layout/index.ts
+++ packages/ui/src/layout/index.ts
@@ -1,2 +1,1 @@
 export * from './apiEffect'
-export * from './user'
--- packages/ui/src/layout/user/index.ts
+++
@@ -1,4 +0,0 @@
-import UserLayout, { UserLayoutContext, UserLayoutContextProps } from './UserLayout'
-
-export { UserLayout, UserLayoutContext }
-export type { UserLayoutContextProps }
