dbe075d02f58602ace26bd01d62d3b93ba31e432
xupengfei <faberxu@gmail.com>
2023-01-03 17:32:53 +0800

    u

--- packages/ui/src/layout/index.ts
+++ packages/ui/src/layout/index.ts
@@ -1,3 +1,2 @@
 export * from './apiEffect'
-export * from './menu'
 export * from './user'
--- packages/ui/src/layout/menu/index.ts
+++
@@ -1,5 +0,0 @@
-import MenuLayout, { MenuLayoutProps } from "./MenuLayout";
-import MenuLayoutContext, { MenuLayoutContextProps } from "./context/MenuLayoutContext";
-
-export {MenuLayout, MenuLayoutContext}
-export type {MenuLayoutProps, MenuLayoutContextProps}
