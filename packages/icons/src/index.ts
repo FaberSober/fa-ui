00506f84475d244ff2697955e91dea5a35d87e1c
xupengfei <faberxu@gmail.com>
2023-01-06 10:47:57 +0800

    feat: fa/icons

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,9 +1,5 @@
-export * from '@/components';
-export * from '@/hooks';
-export * from '@/layout';
-export * from '@/services';
-export * from '@/types';
-export * from '@/utils';
+export * from './components'
+
 
 // 添加fontawesome icons图标
 console.log('添加fontawesome icons图标');
