feda0461e0d290445396c2a4df154d4fd9cce4b0
xupengfei <faberxu@gmail.com>
2023-02-02 16:25:03 +0800

    feat: kgcesi-oauth

--- packages/ui/src/utils/cache.ts
+++ packages/ui/src/utils/cache.ts
@@ -11,3 +11,23 @@ export function setToken(token: string) {
 export function clearToken() {
   localStorage.removeItem(Fa.Constant.TOKEN_KEY);
 }
+
+
+const LOGIN_MODE_KEY = "LOGIN_MODE"
+
+export enum LoginMode {
+  LOCAL = '1',
+  CAS = '2',
+}
+
+export function getLoginMode(): LoginMode | null {
+  return localStorage.getItem(LOGIN_MODE_KEY) as LoginMode;
+}
+
+export function setLoginMode(mode: LoginMode) {
+  localStorage.setItem(LOGIN_MODE_KEY, mode);
+}
+
+export function clearLoginMode() {
+  localStorage.removeItem(LOGIN_MODE_KEY);
+}
