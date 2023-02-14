630d0ad970f97dbe19b02b51c84ae5d32f922dfa
xupengfei <faberxu@gmail.com>
2023-02-14 17:35:18 +0800

    feat: disk-文件增加备注字段

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -4,6 +4,7 @@ export { default as clearForm } from './clearForm';
 export { default as useHeight } from './useHeight';
 export { default as useDelete } from './useDelete';
 export { default as useExport } from './useExport';
+export { default as useViewItem } from './useViewItem';
 
 export { useTableQueryParams }
 
