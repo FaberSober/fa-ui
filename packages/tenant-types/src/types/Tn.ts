bf3a2d83f6410623f20b09b6bff250500301ade6
xu.pengfei <1508075252@qq.com>
2023-03-10 14:13:16 +0800

    feat: tenant-租户系统

---
+++ packages/tenant-types/src/types/Tn.ts
@@ -0,0 +1,148 @@
+import { Fa, FaEnums } from '@fa/ui';
+import TnEnums from './TnEnums'
+
+namespace Tn {
+  /** 租户-租户表 */
+  export interface Tenant extends Fa.BaseDelEntity {
+    /** ID */
+    id: number;
+    /** 租户名称 */
+    name: string;
+    /** 租户状态[1-正常2-停用] */
+    status: TnEnums.TenantStatusEnum;
+    /** 所属用户ID */
+    ownerId: number;
+    /** 所属用户 */
+    ownerName: string;
+    /** 所属用户 */
+    owner: TenantUser;
+  }
+
+  /** 租户-用户表 */
+  export interface TenantUser extends Fa.BaseDelEntity {
+    /** ID */
+    id: number;
+    /** 名称 */
+    name: string;
+    /** 状态[1-正常2-停用] */
+    status: TnEnums.TenantStatusEnum;
+    /** 手机号 */
+    tel: string;
+    /** 密码 */
+    password: string;
+    /** 当前选择的企业ID */
+    corpId: number;
+    /** 租户ID */
+    tenantId: number;
+    /** 当前选择的企业ID */
+    corpName: number;
+    /** 租户ID */
+    tenantName: number;
+  }
+
+  /** 租户-企业表 */
+  export interface TenantCorp extends Fa.BaseDelEntity {
+    /** ID */
+    id: number;
+    /** 名称 */
+    name: string;
+    /** 状态[1-正常2-停用] */
+    status: TnEnums.TenantStatusEnum;
+    /** 租户ID */
+    tenantId: number;
+    /** 租户ID */
+    tenantName: number;
+  }
+
+  /** 租户-企业用户表 */
+  export interface TenantCorpUser extends Fa.TnBaseDelEntity {
+    /** ID */
+    id: number;
+    /** 租户用户ID */
+    userId: string;
+    /** 状态[1-正常2-停用] */
+    status: TnEnums.TenantStatusEnum;
+  }
+
+  /** 租户-URL请求日志 */
+  export interface TenantLogApi extends Fa.TnBaseDelEntity {
+    /** ID */
+    id: string;
+    /** 模块 */
+    biz: string;
+    /** 操作 */
+    opr: string;
+    /** CRUD类型 */
+    crud: FaEnums.LogCrudEnum;
+    /** 请求URL */
+    url: string;
+    /** 请求类型 */
+    method: string;
+    /** 访问客户端 */
+    agent: string;
+    /** 操作系统 */
+    os: string;
+    /** 浏览器 */
+    browser: string;
+    /** 浏览器版本 */
+    version: string;
+    /** 客户端来源 */
+    faFrom: string;
+    /** 客户端版本号 */
+    versionCode: string;
+    /** 客户端版本名 */
+    versionName: string;
+    /** 是否为移动终端 */
+    mobile: boolean;
+    /** 请求花费时间 */
+    duration: number;
+    /** 省 */
+    pro: string;
+    /** 市 */
+    city: string;
+    /** 地址 */
+    addr: string;
+    /** 请求内容 */
+    request: string;
+    /** 请求体大小 */
+    reqSize: number;
+    /** 返回内容 */
+    response: string;
+    /** 返回内容大小 */
+    retSize: number;
+    /** 返回码 */
+    retStatus: string;
+    /** 企业名称 */
+    corpName: string;
+    /** 租户名称 */
+    tenantName: string;
+  }
+
+  /** 租户-登录日志 */
+  export interface TenantLogLogin extends Fa.TnBaseDelEntity {
+    /** ID */
+    id: number;
+    /** 访问客户端 */
+    agent: string;
+    /** 操作系统 */
+    os: string;
+    /** 浏览器 */
+    browser: string;
+    /** 浏览器版本 */
+    version: string;
+    /** 是否为移动终端 */
+    mobile: boolean;
+    /** 省 */
+    pro: string;
+    /** 市 */
+    city: string;
+    /** 地址 */
+    addr: string;
+    /** 企业名称 */
+    corpName: string;
+    /** 租户名称 */
+    tenantName: string;
+  }
+}
+
+export default Tn;
---
+++ packages/tenant-types/src/types/TnEnums.ts
@@ -0,0 +1,27 @@
+namespace TnEnums {
+  // ------------------------------------ BASE ------------------------------------
+  export enum TenantStatusEnum {
+    NORMAL = 1,
+    STOP = 2,
+  }
+
+  // ------------------------------------ RBAC ------------------------------------
+  export enum TenantRbacMenuLevelEnum {
+    APP = 0,
+    MENU = 1,
+    BUTTON = 9,
+  }
+
+  export const TenantRbacMenuLevelEnumMap = {
+    [TenantRbacMenuLevelEnum.APP]: '模块',
+    [TenantRbacMenuLevelEnum.MENU]: '菜单',
+    [TenantRbacMenuLevelEnum.BUTTON]: '按钮',
+  };
+
+  export enum TenantRbacLinkTypeEnum {
+    INNER = 1,
+    OUT = 2,
+  }
+}
+
+export default TnEnums;
