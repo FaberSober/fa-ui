026fe26b9e9eab0c800556d34d605727622d99fa
xu.pengfei <1508075252@qq.com>
2023-03-21 09:58:05 +0800

    feat: tenant-scrm-刷新suite_ticket

--- packages/ui/src/components/decorator/index.ts
+++ packages/ui/src/components/decorator/index.ts
@@ -3,5 +3,6 @@ import FaFullscreenBtn from './FaFullscreenBtn';
 import FaHref from './FaHref';
 import FaLabel from './FaLabel';
 import FaLink from './FaLink';
+import FaClickCopyLink from './FaClickCopyLink';
 
-export { AuthDelBtn, FaFullscreenBtn, FaHref, FaLabel, FaLink };
+export { AuthDelBtn, FaFullscreenBtn, FaHref, FaLabel, FaLink, FaClickCopyLink };
