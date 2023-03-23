d2f0664625596549c6aa078dfdd8e5d3883a7452
xu.pengfei <1508075252@qq.com>
2023-03-23 15:02:59 +0800

    feat: tenant-授权企业

--- packages/tenant-types/src/types/Tn.ts
+++ packages/tenant-types/src/types/Tn.ts
@@ -48,10 +48,66 @@ namespace Tn {
     name: string;
     /** 状态[1-正常2-停用] */
     status: TnEnums.TenantStatusEnum;
+    /** 企业微信ID */
+    wxCorpid: string;
+    /** 第三方服务商企业id */
+    openWxCorpid: string;
+    /** 企业代码(企业统一社会信用代码) */
+    socialCode: string;
+    /** 企业通讯录secret */
+    employeeSecret: string;
+    /** 事件回调地址 */
+    eventCallback: string;
+    /** 企业外部联系人secret */
+    contactSecret: string;
+    /** 回调token */
+    token: string;
+    /** 回调消息加密串 */
+    encodingAesKey: string;
+    /** 授权类型 1-内部开发+自建应用 2-内部开发+自建应用代开发 3-第三方应用授权+自建应用代开发 4代开发应用 */
+    authType: string;
     /** 租户ID */
     tenantId: string;
+  }
+
+  /** 租户-企业应用表 */
+  export interface TenantCorpAgent extends Fa.BaseDelEntity {
+    /** ID */
+    id: string;
+    /** 状态[1-正常2-停用] */
+    status: TnEnums.TenantStatusEnum;
+    /** 微信应用ID */
+    wxAgentId: string;
+    /** 微信应用secret */
+    wxSecret: string;
+    /** 应用名称 */
+    name: string;
+    /** 应用方形头像 */
+    squareLogoUrl: string;
+    /** 应用详情 */
+    description: string;
+    /** 应用是否被停用 0否1是 */
+    close: boolean;
+    /** 应用可信域名 */
+    redirectDomain: string;
+    /** 应用是否打开地理位置上报 0：不上报；1：进入会话上报； */
+    reportLocationFlag: boolean;
+    /** 是否上报用户进入应用事件。0：不接收；1：接收 */
+    isReportenter: boolean;
+    /** 应用主页url */
+    homeUrl: string;
+    /** 应用类型 1-侧边栏 2-应用提醒 3-工作台 */
+    type: number;
+    /** 是否自建应用代开发 0 - 不是 1-是 */
+    isCustomizedApp: boolean;
+    /** 应用权限信息 */
+    privilege: string;
+    /** 代开发应用发布状态 1：未上线；2：已上线 */
+    customizedPublishStatus: boolean;
+    /** 企业ID */
+    corpId: string;
     /** 租户ID */
-    tenantName: string;
+    tenantId: string;
   }
 
   /** 租户-企业用户表 */
