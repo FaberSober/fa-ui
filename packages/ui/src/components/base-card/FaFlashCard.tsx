cae10691b03239b0c878c6788ef16a41de27035f
xupengfei <faberxu@gmail.com>
2023-06-07 11:14:23 +0800

    feat: base-界面优化

--- packages/ui/src/components/base-card/FaFlashCard.tsx
+++ packages/ui/src/components/base-card/FaFlashCard.tsx
@@ -3,7 +3,8 @@ import './FaFlashCard.css'
 
 
 export interface FaFlashCardProps {
-  title: string;
+  hideTitle?: boolean;
+  title?: string;
   extra?: string|ReactNode;
   style?: CSSProperties;
   titleStyle?: CSSProperties;
@@ -15,16 +16,18 @@ export interface FaFlashCardProps {
  * @author xu.pengfei
  * @date 2023/5/4 15:38
  */
-export default function FaFlashCard({title, extra, style, titleStyle, bodyStyle, children}: FaFlashCardProps) {
+export default function FaFlashCard({hideTitle = false, title, extra, style, titleStyle, bodyStyle, children}: FaFlashCardProps) {
   return (
     <div className="fa-flash-card" style={style}>
-      <div className="fa-flash-card-title-div" style={{ ...titleStyle }}>
-        <div className="fa-flash-card-flash-bar" />
-        <div className="fa-flash-card-title">
-          {title}
+      {!hideTitle && (
+        <div className="fa-flash-card-title-div" style={{ ...titleStyle }}>
+          <div className="fa-flash-card-flash-bar" />
+          <div className="fa-flash-card-title">
+            {title}
+          </div>
+          <div className="fa-flash-card-title-extra">{extra}</div>
         </div>
-        <div className="fa-flash-card-title-extra">{extra}</div>
-      </div>
+      )}
       <div className="fa-flash-card-body" style={bodyStyle}>
         {children}
       </div>
