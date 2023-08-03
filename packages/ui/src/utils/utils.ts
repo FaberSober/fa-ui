5bcfd0f67647b4b7821e57c998741455be4907d2
xupengfei <faberxu@gmail.com>
2023-08-03 11:28:09 +0800

    HZX-设备点位-增加字段：options、min、max、unit、alertProp、alertValue

--- packages/ui/src/utils/utils.ts
+++ packages/ui/src/utils/utils.ts
@@ -428,6 +428,7 @@ export function sizeToHuman(size: number, base = 2): string {
 export const REGEX_TEL_NO = /^(13[0-9]|14[5-9]|15[012356789]|166|17[0-8]|18[0-9]|19[8-9])[0-9]{8}$/;
 export const REGEX_CHAR_NUM = /^[0-9a-zA-Z]+$/;
 export const formItemFullLayout = {labelCol: {span: 4}, wrapperCol: {span: 20}};
+export const formItemFullHalfLayout = {labelCol: {span: 4}, wrapperCol: {span: 8}};
 export const formItemHalfLayout = {labelCol: {span: 8}, wrapperCol: {span: 16}};
 
 export const FormRules = {
