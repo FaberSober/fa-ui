a424790d47aec21203150dfc8545b7a61dad470d
xupengfei <faberxu@gmail.com>
2023-11-06 10:05:50 +0800

    ui-FaLabel

--- packages/ui/src/components/decorator/FaLabel.tsx
+++ packages/ui/src/components/decorator/FaLabel.tsx
@@ -7,26 +7,32 @@ export interface FaLabelProps {
   style?: CSSProperties;
   textStyle?: CSSProperties;
   block?: boolean; // 是否块式
+  extra?: React.ReactNode;
 }
 
 /**
  * @author xu.pengfei
  * @date 2021/6/16
  */
-export default function FaLabel({ title, barColor, className, style, textStyle, block }: FaLabelProps) {
+export default function FaLabel({ title, barColor = '#00878b', className, style, textStyle, block, extra }: FaLabelProps) {
   return (
-    <div
-      style={{
-        display: block ? 'flex' : 'inline-flex',
-        alignItems: 'center',
-        padding: '4px 12px',
-        borderLeft: `4px solid #00878b`,
-        backgroundColor: 'var(--fa-bg-color)',
-        ...style,
-      }}
-      className={className}
-    >
-      <div style={{ fontSize: '15px', fontWeight: 600, color: 'var(--fa-text-color)', ...textStyle }}>{title}</div>
+    <div className={className ? ['fa-flex-row-center', className].join(' ') : 'fa-flex-row-center'}>
+      <div
+        style={{
+          display: block ? 'flex' : 'inline-flex',
+          alignItems: 'center',
+          padding: '4px 12px',
+          borderLeft: `4px solid ${barColor}`,
+          backgroundColor: 'var(--fa-bg-color)',
+          ...style,
+        }}
+      >
+        <div style={{ fontSize: '15px', fontWeight: 600, color: 'var(--fa-text-color)', ...textStyle }}>{title}</div>
+      </div>
+      <div className="fa-flex-1" />
+      <div>
+        {extra}
+      </div>
     </div>
   );
 }
