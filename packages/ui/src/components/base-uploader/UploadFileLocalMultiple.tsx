fe5a4d5d212c3bb1423cdeaa00f7b8fb360e938c
xupengfei <faberxu@gmail.com>
2023-11-15 15:40:33 +0800

    beam-智能排单-梁片生产周期管理-钢筋加工

--- packages/ui/src/components/base-uploader/UploadFileLocalMultiple.tsx
+++ packages/ui/src/components/base-uploader/UploadFileLocalMultiple.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { find } from 'lodash';
-import { Button, Upload, UploadProps } from 'antd';
+import { Button, Image, Upload, UploadProps } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { fileSaveApi } from '@ui/services/base';
 import { genAuthHeaders } from '@ui/utils/cache';
@@ -25,6 +25,10 @@ export default function UploadFileLocalMultiple({ children, description, onChang
   const [loading, setLoading] = useState(false);
   const [array, setArray] = useState<any[]>([]); // 文件列表
 
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+  const [srcImage, setSrcImage] = useState('');
+
   useEffect(() => {
     if (value === undefined || value === null) return;
 
@@ -37,6 +41,7 @@ export default function UploadFileLocalMultiple({ children, description, onChang
         size: i.size,
         name: i.originalFilename,
         url: fileSaveApi.genLocalGetFile(i.id),
+        previewUrl: fileSaveApi.genLocalGetFilePreview(i.id),
         status: 'done', // 状态有：uploading done error removed，被 beforeUpload 拦截的文件没有 status 属性
       }));
       setArray(fs);
@@ -71,6 +76,24 @@ export default function UploadFileLocalMultiple({ children, description, onChang
     }
   }
 
+  const uploadImg = props.listType === 'picture-card' ? true : false;
+
+  const handlePreview = async (file: any) => {
+    console.log('handlePreview', file)
+    if (uploadImg) {
+      setPreviewImage(file.previewUrl);
+      setSrcImage(file.url);
+      setPreviewOpen(true);
+    } else {
+      window.open(file.url)
+    }
+  };
+
+  // let canUpload = true;
+  // if (props.maxCount) {
+  //   canUpload = array.length < props.maxCount;
+  // }
+
   return (
     <Upload
       name="file"
@@ -79,11 +102,17 @@ export default function UploadFileLocalMultiple({ children, description, onChang
       onChange={handleOnChange}
       fileList={array}
       multiple
+      onPreview={handlePreview}
       {...props}
     >
-      {children ? (
-        <span>{children}</span>
-      ) : (
+      {children && <span>{children}</span>}
+      {children === undefined && uploadImg && (
+        <div>
+          <UploadOutlined />
+          <div>选择图片</div>
+        </div>
+        )}
+      {children === undefined && !uploadImg && (
         <>
           <Button loading={loading}>
             <UploadOutlined /> 选择文件
@@ -91,6 +120,21 @@ export default function UploadFileLocalMultiple({ children, description, onChang
           {description && <span>{description}</span>}
         </>
       )}
+
+      <div style={{ display: 'none' }}>
+        <Image
+          width={200}
+          style={{ display: 'none' }}
+          src={previewImage}
+          preview={{
+            visible: previewOpen,
+            src: srcImage,
+            onVisibleChange: (value) => {
+              setPreviewOpen(value);
+            },
+          }}
+        />
+      </div>
     </Upload>
   );
 }
