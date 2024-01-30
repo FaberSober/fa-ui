479ff56a8c7ccda5d5db797b381aa9b1cdf3eb9c
xupengfei <faberxu@gmail.com>
2024-01-30 16:41:31 +0800

    base-代码整理

--- packages/ui/src/components/biz-user-select/UserSearchSelect.tsx
+++ packages/ui/src/components/biz-user-select/UserSearchSelect.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BaseSearchSelect, BaseSearchSelectProps } from '@ui/components/base-search-select';
-import {userApi} from '@ui/services/base';
 import { Admin } from '@ui/types';
+import { userApi } from '@ui/services/base';
 
 export interface UserSearchSelectProps extends Omit<BaseSearchSelectProps<Admin.User, string>, 'serviceApi'> {}
 
