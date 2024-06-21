0dc7670ff741bed602464a531ac16d4d77745258
xupengfei <faberxu@gmail.com>
2024-06-22 01:36:23 +0800

    ui-DictEnumApiCheckbox.tsx

--- packages/ui/src/components/base-dict/index.ts
+++ packages/ui/src/components/base-dict/index.ts
@@ -4,6 +4,7 @@ import DictEnumSelector from './DictEnumSelector';
 import DictEnumApiSelector from './DictEnumApiSelector';
 import DictEnumApiRadio from './DictEnumApiRadio';
 import DictEnumApiShow from './DictEnumApiShow';
+import DictEnumApiCheckbox from './DictEnumApiCheckbox';
 import DictDataRadio from './DictDataRadio';
 import DictDataShow from './DictDataShow';
 import BaseBoolRadio from './BaseBoolRadio';
@@ -19,6 +20,7 @@ export {
   DictEnumApiShow,
   DictDataRadio,
   DictDataShow,
+  DictEnumApiCheckbox,
   BaseBoolRadio,
   BaseBoolSelector,
   IntBoolSelector,
