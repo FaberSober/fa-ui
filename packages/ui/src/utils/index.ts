cb9a525972fe34446fe0cb828b9c64b79ffe9e10
xu.pengfei <1508075252@qq.com>
2024-09-11 22:05:41 +0800

    ui-cipher.ts utils

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,8 +1,9 @@
 import * as FaUtils from "./utils"
+import * as FaCipher from "./cipher"
 export * from './cache';
 export * from './request';
 export * from './api-loaders';
 export * from './treeUtils';
 export * as treeUtils from './treeUtils';
 
-export { FaUtils }
+export { FaUtils, FaCipher }
