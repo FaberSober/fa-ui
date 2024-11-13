91ec98855ca07236106e8bf4a062d9193f2885c5
xu.pengfei <1508075252@qq.com>
2024-11-13 15:55:11 +0800

    base-update

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -5,5 +5,6 @@ export * from './request';
 export * from './api-loaders';
 export * from './treeUtils';
 export * as treeUtils from './treeUtils';
+export * as dateUtils from './dateUtils';
 
 export { FaUtils, FaCipher }
