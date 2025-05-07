bb8e683ccc43988b52570a0023e77533608d2349
xu.pengfei <1508075252@qq.com>
2025-05-07 14:44:00 +0800

    ui-catch axios error

--- packages/ui/src/services/core/BaseZeroApi.ts
+++ packages/ui/src/services/core/BaseZeroApi.ts
@@ -12,13 +12,13 @@ export default class BaseZeroApi {
   }
 
   protected get = <E>(api: string, params?: any, config?: AxiosRequestConfig) =>
-    requestGet<E>(`${this.apiPrefix}/${this.apiModal}/${api}`, { ...config, params });
+    requestGet<E>(`${this.apiPrefix}/${this.apiModal}/${api}`, { ...config, params }).catch(e => {console.log('get', e)});
 
   protected delete = <E>(api: string, config?: AxiosRequestConfig) =>
-    requestDelete<E>(`${this.apiPrefix}/${this.apiModal}/${api}`, config);
+    requestDelete<E>(`${this.apiPrefix}/${this.apiModal}/${api}`, config).catch(e => {console.log('delete', e)});
 
   protected post = <E>(api: string, body: object, config?: AxiosRequestConfig) =>
-    requestPost<E>(`${this.apiPrefix}/${this.apiModal}/${api}`, body, config);
+    requestPost<E>(`${this.apiPrefix}/${this.apiModal}/${api}`, body, config).catch(e => {console.log('post', e)});
 
   protected postFile = <E>(api: string, file: any, config?: AxiosRequestConfig) => {
     const formData = new FormData();
@@ -27,21 +27,21 @@ export default class BaseZeroApi {
       timeout: -1,
       ...config,
       headers: { 'Content-type': 'multipart/form-data' },
-    });
+    }).catch(e => {console.log('postFile', e)});
   };
 
   protected postForm = <E>(api: string, formData: any, config?: AxiosRequestConfig) => {
     return requestPost<E>(`${this.apiPrefix}/${this.apiModal}/${api}`, formData, {
       ...config,
       headers: { 'Content-type': 'multipart/form-data' },
-    });
+    }).catch(e => {console.log('postForm', e)});
   };
 
   protected put = <E>(api: string, body: object, config?: AxiosRequestConfig) =>
-    requestPut<E>(`${this.apiPrefix}/${this.apiModal}/${api}`, body, config);
+    requestPut<E>(`${this.apiPrefix}/${this.apiModal}/${api}`, body, config).catch(e => {console.log('put', e)});
 
   protected download = (api: string, body: object, config?: AxiosRequestConfig) =>
-    requestDownload(`${this.apiPrefix}/${this.apiModal}/${api}`, body, config);
+    requestDownload(`${this.apiPrefix}/${this.apiModal}/${api}`, body, config).catch(e => {console.log('download', e)});
 
   getUrl = (api: string) => `${this.apiPrefix}/${this.apiModal}/${api}`;
 }
