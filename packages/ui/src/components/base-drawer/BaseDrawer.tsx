096feb3ffc4f61e60ef734e910a77f010aa4668d
xu.pengfei <1508075252@qq.com>
2025-06-04 15:22:11 +0800

    ui-FaResizeHorizontal增加transition处理，增加拖动时设置元素不可选中文本； BaseDrawer-增加FaResizeHorizontal支持拖动控制宽度；

--- packages/ui/src/components/base-drawer/BaseDrawer.tsx
+++ packages/ui/src/components/base-drawer/BaseDrawer.tsx
@@ -1,5 +1,6 @@
-import React, {createContext, ReactNode, useState} from 'react';
+import React, { createContext, ReactNode, useEffect, useState } from 'react';
 import {Drawer, DrawerProps} from "antd";
+import { FaResizeHorizontal } from "@ui/components";
 
 export interface BaseDrawerContextProps {
   closeDrawer: () => void;
@@ -8,6 +9,7 @@ export interface BaseDrawerContextProps {
 export const BaseDrawerContext = createContext<BaseDrawerContextProps>({ closeDrawer: () => {} });
 
 export interface BaseDrawerProps extends DrawerProps {
+  hideResize?: boolean;
   triggerDom?: ReactNode;
 }
 
@@ -15,8 +17,17 @@ export interface BaseDrawerProps extends DrawerProps {
  * @author xu.pengfei
  * @date 2022/12/28 10:41
  */
-export default function BaseDrawer({children, triggerDom, bodyStyle, onClose, ...props }: BaseDrawerProps) {
+export default function BaseDrawer({children, hideResize = false, triggerDom, bodyStyle, onClose, ...props }: BaseDrawerProps) {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      const dom = document.getElementById("fa-drawer")
+      if (dom) {
+        dom.parentElement?.setAttribute("id", "fa-drawer-content")
+      }
+    }
+  }, [open])
 
   return (
     <BaseDrawerContext.Provider value={{closeDrawer: () => setOpen(false)}}>
@@ -31,9 +42,15 @@ export default function BaseDrawer({children, triggerDom, bodyStyle, onClose, ..
           }}
           width={700}
           styles={{ body: { position: 'relative', ...bodyStyle } }}
+          id="fa-drawer"
           {...props}
         >
-          {(open || props.forceRender) && children}
+          {(open || props.forceRender) && (
+            <>
+              {!hideResize && <FaResizeHorizontal domId="fa-drawer-content" position="left" style={{left: 0}} minWidth={200} />}
+              {children}
+            </>
+          )}
         </Drawer>
       </span>
     </BaseDrawerContext.Provider>
--- packages/ui/src/components/base-resize/FaResizeHorizontal.tsx
+++ packages/ui/src/components/base-resize/FaResizeHorizontal.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { CSSProperties, useCallback } from "react";
 import { FaUtils } from "@ui/utils";
 
 
@@ -7,6 +7,7 @@ let deltaX = 0;
 let startX = 0;
 let initWidth = 0;
 let dom:HTMLElement|null = null;
+let domTransition = '';
 
 export interface FaResizeHorizontalProps {
   domId: string;
@@ -16,6 +17,7 @@ export interface FaResizeHorizontalProps {
   maxWidth?: number;
   children?: React.ReactNode;
   onWidthChange?: (v:number) => void;
+  style?: CSSProperties;
 }
 
 /**
@@ -27,7 +29,7 @@ export interface FaResizeHorizontalProps {
  * @param children
  * @constructor
  */
-export default function FaResizeHorizontal({ domId, position, minWidth, maxWidth, onWidthChange, children }: FaResizeHorizontalProps) {
+export default function FaResizeHorizontal({ domId, position, minWidth, maxWidth, onWidthChange, style, children }: FaResizeHorizontalProps) {
 
   const mouseMoveListener:any = useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (!drag) return;
@@ -57,6 +59,11 @@ export default function FaResizeHorizontal({ domId, position, minWidth, maxWidth
     drag = false
     window.removeEventListener('mousemove', mouseMoveListener)
     window.removeEventListener('mouseup', mouseUpListener)
+    if (dom) {
+      // 恢复元素的transition
+      dom.style.transition = domTransition
+      dom.classList.remove('fa-drag-area-no-select'); // 恢复选中
+    }
   }, [])
 
   return (
@@ -72,13 +79,20 @@ export default function FaResizeHorizontal({ domId, position, minWidth, maxWidth
         height: '1.5rem',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        ...style,
     }}
       onMouseDown={(e) => {
         drag = true
         startX = e.clientX;
         deltaX = 0
         dom = document.getElementById(domId)
+        if (dom) {
+          // 计算并保存元素的transition
+          domTransition = window.getComputedStyle(dom).transition
+          dom.style.transition = 'none'
+          dom.classList.add('fa-drag-area-no-select'); // 启用禁止选中
+        }
         initWidth = FaUtils.getDomRectById(domId)!.width
         window.addEventListener('mousemove', mouseMoveListener)
         window.addEventListener('mouseup', mouseUpListener)
