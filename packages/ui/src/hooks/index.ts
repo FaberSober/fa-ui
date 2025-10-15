96b15e0f016629f0fbb239dc45106d43564c3be8
xu.pengfei <1508075252@qq.com>
2025-10-15 15:12:10 +0800

    add useDeleteConfirm

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -4,6 +4,7 @@ export { default as clearForm } from './clearForm';
 export { default as clearFormExclude } from './clearFormExclude';
 export { default as useHeight } from './useHeight';
 export { default as useDelete } from './useDelete';
+export { default as useDeleteConfirm } from './useDeleteConfirm';
 export { default as useDeleteByQuery } from './useDeleteByQuery';
 export { default as useExport } from './useExport';
 export { default as useExportBase } from './useExportBase';
