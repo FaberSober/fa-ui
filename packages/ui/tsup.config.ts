de59d139f64efbea6918fac5e7c3bae6e08b162d
xu.pengfei <1508075252@qq.com>
2025-12-04 16:59:37 +0800

    update

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  splitting: false,
+  // splitting: false,
   sourcemap: true,
   // clean: true,
   dts: true,
   watch: false,
   format: ['esm', 'cjs'],
+  target: 'es2022',
+  platform: 'browser',
+  clean: true,
+  shims: true,                    // 关键！修复 cjs 下 __dirname 等问题
+  splitting: true,                // 开启代码分割（对大组件库很有效）
+  banner: {
+    js: "'use client';"
+  },
   external: [
     '@tinymce/tinymce-react',
     'axios',
@@ -19,9 +27,15 @@ export default defineConfig({
     'lodash',
     'qs',
     'react',
+    'react-dom',
     'react-router-dom',
     'react-contexify',
     'uuid',
     'use-bus',
   ],
+  esbuildOptions(options) {
+    options.alias = {
+      '@ui': './src',
+    };
+  },
 });
