1b1c83802bf994c06dd6085235ec07f416192640
xu.pengfei <1508075252@qq.com>
2026-01-21 17:16:45 +0800

    add icon

--- packages/icons/src/components/custom/index.ts
+++ packages/icons/src/components/custom/index.ts
@@ -3,5 +3,6 @@ export * from './FaIconInclusive'
 export * from './FaIconRoute'
 export * from './FaIconSend'
 export * from './FaIconSlider'
+export * from './FaIconSubFlow'
 export * from './FaIconTrigger'
 
