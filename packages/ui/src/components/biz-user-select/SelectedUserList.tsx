cfc5ac080162de461dbcf591d2c97735890795ce
xu.pengfei <1508075252@qq.com>
2026-09-19 22:20:15 +0800

    feat: cache selected user details

--- packages/ui/src/components/biz-user-select/SelectedUserList.tsx
+++ packages/ui/src/components/biz-user-select/SelectedUserList.tsx
@@ -1,8 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {userApi} from "@ui/services/base";
 import {Admin} from "@ui/types";
 import {Button, Space, Table} from "antd";
-import {isNil} from "lodash";
 import {SelectedUser} from "./BizUserSelect";
 
 
@@ -17,16 +16,49 @@ export interface SelectedUserListProps {
  */
 export default function SelectedUserList({selectedUsers, onRemove}: SelectedUserListProps) {
   const [array, setArray] = useState<Admin.User[]>([]);
+  const userCacheRef = useRef<Record<string, Admin.User>>({});
+  const pendingIdsRef = useRef(new Set<string>());
+  const selectedIdsRef = useRef<string[]>([]);
+  const mountedRef = useRef(false);
+
+  function resolveUsers(ids: string[]) {
+    return ids
+      .map((id) => userCacheRef.current[id])
+      .filter((user): user is Admin.User => Boolean(user));
+  }
 
   useEffect(() => {
-    // console.log('selectedUsers', selectedUsers)
-    if (isNil(selectedUsers) || selectedUsers.length === 0) {
-      setArray([])
-      return;
-    }
-    userApi.list({query: {'id#$in': selectedUsers.map(i => i.id)}}).then(res => {
-      setArray(res.data)
-    })
+    mountedRef.current = true;
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    const selectedIds = Array.from(new Set((selectedUsers || []).map((item) => item.id)));
+    selectedIdsRef.current = selectedIds;
+    setArray(resolveUsers(selectedIds));
+
+    const missingIds = selectedIds.filter(
+      (id) => !userCacheRef.current[id] && !pendingIdsRef.current.has(id),
+    );
+    if (missingIds.length === 0) return;
+    missingIds.forEach((id) => pendingIdsRef.current.add(id));
+
+    const clearPendingIds = () => {
+      missingIds.forEach((id) => pendingIdsRef.current.delete(id));
+    };
+
+    userApi.list({query: {'id#$in': missingIds}}).then((res) => {
+      res.data.forEach((user) => {
+        userCacheRef.current[user.id] = user;
+      });
+      clearPendingIds();
+      if (mountedRef.current) setArray(resolveUsers(selectedIdsRef.current));
+    }).catch(() => {
+      clearPendingIds();
+      if (mountedRef.current) setArray(resolveUsers(selectedIdsRef.current));
+    });
   }, [selectedUsers])
 
   const disallowRemoveUserIds = (selectedUsers || []).filter(i => !i.allowRemove).map(i => i.id)
